6bcba380f0297adf49acbd4f30054d2cc8313282
kat <katharinebrooks@hotmail.co.uk>
2022-05-05 12:12:46 +0100

    tests for different durations of whole days

--- spec/booking_pricer_spec.rb
+++ spec/booking_pricer_spec.rb
@@ -14,5 +14,14 @@ describe Booking do
     it 'defaults to 0 if given no duration' do
       expect(booking.get_price()).to eq 0
     end
+    it 'calculates price for 1 day' do
+      expect(booking.get_price(1440)).to eq tariff[:daily]
+    end
+    it 'calculates price for 2 days' do
+      expect(booking.get_price(2880)).to eq tariff[:weekly]
+    end
+    it 'calculates price for 3 days' do
+      expect(booking.get_price(4320)).to eq tariff[:weekly]
+    end
   end
 end
